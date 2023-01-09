269ff71db2ac4644863fd8f8de2e4cc539fa9dd9
Keith Mair <mairck@gmail.com>
2023-01-08 22:19:07 -0700

    Reconfig for lesson 2

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-  res.send('Brenda Mair');
-});
+routes.use('/contacts', require('./contacts'));
 
 module.exports = routes;
